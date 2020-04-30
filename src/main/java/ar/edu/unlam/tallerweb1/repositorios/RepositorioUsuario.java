e63a486a12427d40539d3b30babdf56b2f8e6feb
Agustin Franco <francoagustinfacundo@gmail.com>
2020-04-30 20:37:07 -0300

    identado de codigo

--- src/main/java/ar/edu/unlam/tallerweb1/repositorios/RepositorioUsuario.java
+++ src/main/java/ar/edu/unlam/tallerweb1/repositorios/RepositorioUsuario.java
@@ -4,6 +4,5 @@ import ar.edu.unlam.tallerweb1.modelo.Usuario;
 
 // Interface que define los metodos del Repositorio de Usuarios.
 public interface RepositorioUsuario {
-	
-	Usuario consultarUsuario (Usuario usuario);
+	Usuario consultarUsuario(Usuario usuario);
 }
--- src/main/java/ar/edu/unlam/tallerweb1/repositorios/RepositorioUsuarioImpl.java
+++ src/main/java/ar/edu/unlam/tallerweb1/repositorios/RepositorioUsuarioImpl.java
@@ -1,13 +1,12 @@
 package ar.edu.unlam.tallerweb1.repositorios;
 
-import ar.edu.unlam.tallerweb1.modelo.Usuario;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import javax.inject.Inject;
+import ar.edu.unlam.tallerweb1.modelo.Usuario;
 
 // implelemtacion del repositorio de usuarios, la anotacion @Repository indica a Spring que esta clase es un componente que debe
 // ser manejado por el framework, debe indicarse en applicationContext que busque en el paquete ar.edu.unlam.tallerweb1.dao
@@ -15,26 +14,28 @@ import javax.inject.Inject;
 @Repository("repositorioUsuario")
 public class RepositorioUsuarioImpl implements RepositorioUsuario {
 
-	// Como todo repositorio maneja acciones de persistencia, normalmente estará inyectado el session factory de hibernate
+	// Como todo repositorio maneja acciones de persistencia, normalmente estará
+	// inyectado el session factory de hibernate
 	// el mismo está difinido en el archivo hibernateContext.xml
 	private SessionFactory sessionFactory;
 
-    @Autowired
-	public RepositorioUsuarioImpl(SessionFactory sessionFactory){
+	@Autowired
+	public RepositorioUsuarioImpl(SessionFactory sessionFactory) {
 		this.sessionFactory = sessionFactory;
 	}
 
+	@SuppressWarnings("deprecation")
 	@Override
 	public Usuario consultarUsuario(Usuario usuario) {
 
-		// Se obtiene la sesion asociada a la transaccion iniciada en el servicio que invoca a este metodo y se crea un criterio
-		// de busqueda de Usuario donde el email y password sean iguales a los del objeto recibido como parametro
+		// Se obtiene la sesion asociada a la transaccion iniciada en el servicio que
+		// invoca a este metodo y se crea un criterio
+		// de busqueda de Usuario donde el email y password sean iguales a los del
+		// objeto recibido como parametro
 		// uniqueResult da error si se encuentran más de un resultado en la busqueda.
 		final Session session = sessionFactory.getCurrentSession();
-		return (Usuario) session.createCriteria(Usuario.class)
-				.add(Restrictions.eq("email", usuario.getEmail()))
-				.add(Restrictions.eq("password", usuario.getPassword()))
-				.uniqueResult();
+		return (Usuario) session.createCriteria(Usuario.class).add(Restrictions.eq("email", usuario.getEmail()))
+				.add(Restrictions.eq("password", usuario.getPassword())).uniqueResult();
 	}
 
 }
