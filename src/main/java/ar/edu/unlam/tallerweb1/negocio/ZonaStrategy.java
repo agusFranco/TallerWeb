24074f14107c57eb130aa3e6ef19080637824805
Agustin Franco <francoagustinfacundo@gmail.com>
2020-06-02 23:02:04 -0300

    Avanzo con la prioridad zona

--- src/main/java/ar/edu/unlam/tallerweb1/negocio/ZonaStrategy.java
+++ src/main/java/ar/edu/unlam/tallerweb1/negocio/ZonaStrategy.java
@@ -1,17 +1,39 @@
 package ar.edu.unlam.tallerweb1.negocio;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import ar.edu.unlam.tallerweb1.modelo.Establecimiento;
 import ar.edu.unlam.tallerweb1.modelo.Insumo;
+import ar.edu.unlam.tallerweb1.modelo.Zona;
 
 //Estrategia: Calcular prioridad por "ZONA"
 public class ZonaStrategy implements Strategy {
 
 	@Override
 	public List<Establecimiento> calcular(List<Establecimiento> establecimientos) {
+
+		// Establezco una funcion para obtener la zona del establecimiento
+		Function<Establecimiento, Zona> obtenerZona = Establecimiento::getZona;
+
+		// Creo un comparador.
+		Comparator comparator = Comparator.comparing(obtenerZona.andThen(Zona::getPuntaje));
+
+		// Orderno la lista.
+		Collections.sort(establecimientos, comparator.reversed());
+
+		Integer prioridad = 1;
+
+		for (Establecimiento item : establecimientos) {
+			item.setPrioridad((float) prioridad);
+			prioridad++;
+		}
+
 		return establecimientos;
 	}
 
