19f719f2dc990f1369d30162d936594ba1bc5fd5
Sebastian <sebastiantofano@gmail.com>
2020-06-12 23:20:33 -0300

    Test de Servicio de Establecimiento

---
+++ src/test/java/ar/edu/unlam/tallerweb1/serviciostest/ServicioEstablecimientoTest.java
@@ -0,0 +1,39 @@
+package ar.edu.unlam.tallerweb1.serviciostest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import ar.edu.unlam.tallerweb1.SpringTest;
+import ar.edu.unlam.tallerweb1.modelo.Establecimiento;
+import ar.edu.unlam.tallerweb1.repositorios.RepositorioEstablecimiento;
+import ar.edu.unlam.tallerweb1.servicios.ServicioEstablecimientoImpl;
+
+public class ServicioEstablecimientoTest extends SpringTest {
+	
+	@Test
+	public void elServicioEstablecimientoMayorOcupacionMeDevuelveUnEstablecimientoSolo() {
+		//Preparacion
+		RepositorioEstablecimiento servicioEstablecimientoDao = mock(RepositorioEstablecimiento.class);
+		List<Establecimiento> lista = new ArrayList<Establecimiento>();
+		when(servicioEstablecimientoDao.getAll()).thenReturn(lista);
+		lista.add(new Establecimiento());
+		
+		//Ejecucion
+		ServicioEstablecimientoImpl servicioEstablecimiento = new ServicioEstablecimientoImpl(servicioEstablecimientoDao);
+		Establecimiento unicoEstablecimientoDevuelto = servicioEstablecimiento.establecimientoMayorOcupacion();
+		unicoEstablecimientoDevuelto.setNombre("Establecimiento con mayor capacidad");
+		
+		//Validacion
+		assertThat(unicoEstablecimientoDevuelto).isNotNull();
+		
+		
+		
+	}
+
+}
